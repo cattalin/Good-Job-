c98c4eee12d6c395756d2b32565e910f9968dea3
cattalin <catathebest1@yahoo.com>
2017-03-23 15:50:07 +0200

    First Upload

---
+++ BackEnd/server/app.js
@@ -0,0 +1,61 @@
+//Needed prerequisites
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+//port number
+const PORT = 8000 || process.env.PORT;
+//Express environment
+var app = express();
+
+//Connecting to DataBase
+const config = require('./config/database');
+mongoose.connect(config.database);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database '+config.database);
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: '+err);
+});
+
+
+
+// CORS Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+
+//Routing
+const users = require('./routes/users');
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+
+// Start Server
+app.listen(PORT, () => {
+    console.log('Server started on port '+PORT);
+});
+
---
+++ BackEnd/server/config/database.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
+  database: 'mongodb://localhost:27017/fromscratch',    //dev
+  secret: 'yoursecret'
+}
