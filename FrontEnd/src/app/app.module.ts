2e61e31152269ba5c383134e29b888e3570bcfb2
cattalin <catathebest1@yahoo.com>
2017-03-26 19:58:04 +0300

    Revert "Merge pull request #1 from ciprianmacovei/master"
    This reverts commit 0529c431d052304d8af8ec89bbd657a45522a10a, reversing
    changes made to c98c4eee12d6c395756d2b32565e910f9968dea3.

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -7,28 +7,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { AddVideoComponent } from './add-video/add-video.component';
-import { ListVideoComponent } from './list-video/list-video.component';
-import { CommentBoxComponent } from './comment-box/comment-box.component';
-import {CollectableVideos} from "./shared/collectable.service";
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ProfileComponent,
-    AddVideoComponent,
-    ListVideoComponent,
-    CommentBoxComponent
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [CollectableVideos],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
