6cec5da128ab73101a035ac97852d616a80f181c
alinaanghel <anghel.alinaioana@yahoo.com>
2017-04-19 21:15:19 +0300

    Revert "Revert "Profile viewer""
    This reverts commit b151dfe7a9769c6633623581f31852b488e1d480.

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -37,7 +37,8 @@ const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   //{path:'search',component: SearchComponent} -- Ignorati, Sebi nu stie sa adauge componenta la index :)
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: 'upload', component: SubmitVideoComponent, canActivate: [AuthGuard] }
+  { path: 'upload', component: SubmitVideoComponent, canActivate: [AuthGuard] },
+  { path: 'userprofile', component: ViewProfileComponent, canActivate: [AuthGuard] }
 ]
 
 @NgModule({
