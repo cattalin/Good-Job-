c258f193c98ee23dff8ef1a911d75e34f083dd54
Sebastian Axinte <sebastian.axinte.dev@gmail.com>
2017-05-11 19:15:28 +0300

    Removed unused import.

--- FrontEnd/src/app/services/search.service.ts
+++ FrontEnd/src/app/services/search.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers,URLSearchParams, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { VideoComponent } from '../components/video/video.component';
-import { SearchFeedComponent} from '../components/search-feed/search-feed.component';
 import { tokenNotExpired } from 'angular2-jwt';
 import { VideoData } from '../models/video-data';
 import 'rxjs/add/operator/map';
