1ac67432ab10e5d71ee59c4eb84fe03bc29e3e55
cattalin <catathebest1@yahoo.com>
2017-05-23 21:33:41 +0300

    following feed

--- BackEnd/server/config/database.js
+++ BackEnd/server/config/database.js
@@ -1,6 +1,6 @@
 
 module.exports = {
   //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
-  database: 'mongodb://localhost:27017/fromscratch',    //dev
+  database: 'mongodb://79.115.29.173/fromscratch',    //dev
   secret: 'yoursecret'
 }
