d4c0892d93b5dad0aa1c54622a8761758cc090f0
bogdandv <bogdandv_14@yahoo.com>
2017-05-23 22:15:58 +0300

    uj

--- BackEnd/server/config/database.js
+++ BackEnd/server/config/database.js
@@ -2,6 +2,6 @@
 module.exports = {
   //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
   database: 'mongodb://localhost:27017/fromscratch',    //dev
- // database: 'mongodb://79.115.29.173:27017/fromscratch',    //dev
+  //database: 'mongodb://79.115.29.173:27017/fromscratch',    //dev
   secret: 'yoursecret'
 }
