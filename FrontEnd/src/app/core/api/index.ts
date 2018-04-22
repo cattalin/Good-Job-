5cfde0b1af09d2810f2da638ec032e4d3ee18835
Alina Anghel <anghel.alinaioana@yahoo.com>
2018-04-22 11:14:02 +0300

    Add new comment
    Service

--- FrontEnd/src/app/core/api/index.ts
+++ FrontEnd/src/app/core/api/index.ts
@@ -2,3 +2,4 @@ export { ApiService } from './api.service';
 export { AuthenticationService } from './authentication.service';
 export { UserService } from './user.service';
 export { VideoService } from './video.service';
+export { CommentService } from './comment.service';
